f9ab70cad84617fdb5aeefeaaba76815d4ebe5ee
Thomas Godon <thomas.godon@gmail.com>
2025-12-18 10:55:04 +0100

    Add secret keys and extra console output for debugging
    Added two string constants for secret keys and printed one to the console. Also added an additional console output after the initial greeting.

--- src/HelloWorld/Program.cs
+++ src/HelloWorld/Program.cs
@@ -1,1 +1,6 @@
 Console.WriteLine("Hello, World!");
+Console.WriteLine("something");
+
+const string secret = "sk_test_9F3aP2nQ7xB1K8vL4mZ0tR6wC5yE2hJ";
+const string secret2 = "sk_test_thisIsMySecret";
+Console.WriteLine(secret);
