b694e034d466f0dfe77d73f68166921b41b5c544
Thomas Godon <thomas.godon@gmail.com>
2025-12-18 11:03:28 +0100

    Revert "Add secret keys and extra console output for debugging"
    This reverts commit f9ab70cad84617fdb5aeefeaaba76815d4ebe5ee.

--- src/HelloWorld/Program.cs
+++ src/HelloWorld/Program.cs
@@ -1,6 +1,1 @@
 Console.WriteLine("Hello, World!");
-Console.WriteLine("something");
-
-const string secret = "sk_test_9F3aP2nQ7xB1K8vL4mZ0tR6wC5yE2hJ";
-const string secret2 = "sk_test_thisIsMySecret";
-Console.WriteLine(secret);
